3a1b9354163dbb27f5f96392e9f3ff6399a7405f
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2024-02-27 04:23:38 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from helper.database import db
-from pyromod.exceptions import ListenerTimeout
 from config import Txt
 
 
@@ -25,34 +24,3 @@ async def handle_metadata(bot: Client, message: Message):
     return await message.reply_text(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
 
 
-@Client.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
-async def query_metadata(bot: Client, query: CallbackQuery):
-
-    data = query.data
-
-    if data.startswith('metadata_'):
-        _bool = data.split('_')[1]
-        user_metadata = await db.get_metadata_code(query.from_user.id)
-
-        if bool(eval(_bool)):
-            await db.set_metadata(query.from_user.id, bool_meta=False)
-            await query.message.edit(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
-
-        else:
-            await db.set_metadata(query.from_user.id, bool_meta=True)
-            await query.message.edit(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
-
-    elif data == 'cutom_metadata':
-        await query.message.delete()
-        try:
-            try:
-                metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True)
-            except ListenerTimeout:
-                await query.message.reply_text("⚠️ Error!!\n\n**Request timed out.**\nRestart by using /metadata", reply_to_message_id=query.message.id)
-                return
-            print(metadata.text)
-            ms = await query.message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
-            await db.set_metadata_code(query.from_user.id, metadata_code=metadata.text)
-            await ms.edit("**Your Metadta Code Set Successfully ✅**")
-        except Exception as e:
-            print(e)
