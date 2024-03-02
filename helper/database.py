30c437e712b22ae300ae90aefac25af6c44f9f4c
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-03-02 07:03:31 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -17,8 +17,7 @@ class Database:
             file_id=None,
             caption=None,
             format_template=None,
-            metadata=False,
-            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @AnimeZenith" -metadata author= "sensei put your name hahha" -metadata:s:s title="Subtitled By :- my grandmother" -metadata:s:a title="By :- my grandfather 
+            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @AnimeZenith" -metadata author= "sensei put your name hahha" -metadata:s:s title="Subtitled By :- my grandmother" -metadata:s:a title="By :- my grandfather 
        " -metadata:s:v title="By:- my dick" """
         )
 
@@ -76,11 +75,11 @@ class Database:
         user = await self.col.find_one({'_id': int(id)})
         return user.get('media_type', None)
 
-    async def set_metadata(self, id, bool_meta):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
+    async def set_metadata(self, user_id, metadata):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'metadata': metadata}})
 
     async def get_metadata(self, id):
-        user = await self.col.find_one({'_id': int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return user.get('metadata', None)
 
     async def set_metadata_code(self, id, metadata_code):
